e135da425d5df7c5934d27a6010da44d635096ff
Zeeshan Khan <zkhan1093@gmail.com>
2022-04-14 14:33:53 +0530

    adding rc details view

--- crawler.py
+++ crawler.py
@@ -1,27 +1,41 @@
+from functools import lru_cache
 from urllib.error import HTTPError
 import requests
 import logging
 from bs4 import BeautifulSoup
 
 
-def fetch_sale_details(fpsid, month, year, dist_code):
-    # raise Exception("bad exception!")
+def do_request(url, payload):
+
     logging.info("hitting service brace!!!")
+    try:
+        res = requests.post(
+            url,
+            data=payload,
+        )
+    except HTTPError as ex:
+        logging.exception(f"failed to get data from server {url} {payload}")
+    else:
+        return res.text
+
+
+def fetch_sale_details(fpsid, month, year, dist_code):
     payload = {
         "dist_code": dist_code,
         "fps_id": fpsid,
         "month": month,
         "year": year,
     }
-    try:
-        res = requests.post(
-            "http://epos.bihar.gov.in/FPS_Trans_Details.jsp",
-            data=payload,
-        )
-    except HTTPError as ex:
-        logging.exception(f"failed to get sales details for {payload}")
-    else:
-        return res.text
+    return do_request("http://epos.bihar.gov.in/FPS_Trans_Details.jsp", payload)
+
+
+def fetch_rc_details(rc_number, month, year):
+    payload = {
+        "src_no": rc_number,
+        "month": month,
+        "year": year,
+    }
+    return do_request("http://epos.bihar.gov.in/SRC_Trans_Details.jsp", payload)
 
 
 class SalesDetailsParser:
@@ -51,7 +65,40 @@ class SalesDetailsParser:
         return [th.get_text().strip() for th in ths]
 
 
+class RCDetailParser:
+    def parse(self, content):
+        root_bs = BeautifulSoup(content, features="html.parser")
+        tables = root_bs.find_all("table")
+        members = self._parse_members(tables[0])
+        transactons = self._parse_transactions(tables[3])
+        return members, transactons
+
+    def _parse_members(self, table):
+        trs = table.find_all("tr")
+        trs = trs[2:]
+        headers = [th.get_text().strip() for th in trs.pop(0).find_all("th")]
+        rows = [[td.get_text().strip() for td in tr.find_all("td")] for tr in trs]
+        return [dict(zip(headers, row)) for row in rows]
+
+    def _parse_transactions(self, table):
+        trs = table.find_all("tr")
+        trs = trs[1:]
+        tds = trs.pop(0).find_all("td")
+        tds = tds[:-1] + trs.pop(0).find_all("td")
+        headers = [td.get_text().strip() for td in tds]
+        rows = [[td.get_text().strip() for td in tr.find_all("td")] for tr in trs]
+        return [dict(zip(headers, row)) for row in rows]
+
+
+@lru_cache
 def get_sales_details(fpsid=123300100909, month=3, year=2022, dist_code=233):
     table = fetch_sale_details(fpsid=fpsid, month=month, year=year, dist_code=dist_code)
     _, items = SalesDetailsParser().parse(table)
     return items
+
+
+@lru_cache
+def get_rc_details(rc_number=10310060087015900034, month=3, year=2022):
+    content = fetch_rc_details(rc_number=rc_number, month=month, year=year)
+    members, transactions = RCDetailParser().parse(content)
+    return members, transactions
--- server.py
+++ server.py
@@ -15,6 +15,19 @@ app = Flask("app")
 CORS(app)
 
 
+def error_response():
+    _, ex, exc_traceback = sys.exc_info()
+    return (
+        jsonify(
+            dict(
+                error=str(ex),
+                traceback=traceback.format_tb(exc_traceback),
+            )
+        ),
+        400,
+    )
+
+
 @app.route("/")
 def index():
     return app.send_static_file("index.html")
@@ -34,15 +47,24 @@ def get_sales_details():
             fpsid=fpsid, month=month, year=year, dist_code=dist_code
         )
         return jsonify(items)
-    except Exception as ex:
+    except Exception:
         logging.exception("failed to get data")
-        _, _, exc_traceback = sys.exc_info()
-        return (
-            jsonify(
-                dict(
-                    error=str(ex),
-                    traceback=traceback.format_tb(exc_traceback),
-                )
-            ),
-            400,
+        return error_response()
+
+
+@app.route("/get-rc-details")
+def get_rc_details():
+    logging.info("api request")
+    # for Ramkut singh
+    # rcnumber=10310060087015900034, month=3, year=2022
+    rc_number = request.args["rcnumber"]
+    month = request.args["month"]
+    year = request.args["year"]
+    try:
+        members, transactions = crawler.get_rc_details(
+            rc_number=rc_number, month=month, year=year
         )
+        return jsonify(dict(members=members, transactions=transactions))
+    except Exception:
+        logging.exception("failed to get data")
+        return error_response()
