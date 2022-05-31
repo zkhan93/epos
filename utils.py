d1b6cd36c92f213f295b3ede96e63e1bff249684
Zeeshan Khan <zkhan1093@gmail.com>
2022-05-31 18:41:44 +0530

    fix clear cache

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ WEEK_IN_SECONDS = 60 * 24 * 30
 
 
 def get_cache():
-    clear_cache = os.getenv("CLEAR_CACHE", False)
+    clear_cache = os.getenv("CLEAR_CACHE", "False") == "True"
     cache = Cache(directory=os.getenv("CACHE_DIR", "/tmp/cache"))
     if clear_cache:
         logging.info("Clearing Cache...")
