4d02cfb520b4e184df282fe9fdc6b11f7c419c44
Zeeshan Khan <zkhan1093@gmail.com>
2022-05-31 18:40:32 +0530

    added clear cache

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from diskcache import Cache
 
@@ -5,4 +6,10 @@ WEEK_IN_SECONDS = 60 * 24 * 30
 
 
 def get_cache():
-    return Cache(directory=os.getenv("CACHE_DIR", "/tmp/cache"))
+    clear_cache = os.getenv("CLEAR_CACHE", False)
+    cache = Cache(directory=os.getenv("CACHE_DIR", "/tmp/cache"))
+    if clear_cache:
+        logging.info("Clearing Cache...")
+        cache.clear()
+        logging.info("Cache cleared")
+    return cache
