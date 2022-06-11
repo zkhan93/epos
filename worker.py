cca609409c313e133445911fea24fcc51d9fadfb
Zeeshan Khan <zkhan1093@gmail.com>
2022-06-11 15:40:34 +0530

    added default database

--- worker.py
+++ worker.py
@@ -1,11 +1,12 @@
 import os
-
+import logging
 from celery import Celery
 
+logger = logging.getLogger("worker")
 celery = Celery(__name__)
-celery.conf.update(
-    {
-        "broker_url": os.getenv("CELERY_BROKER_URL", "redis://redis:6379"),
-        "result_backend": os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379"),
-    }
-)
+values = {
+    "broker_url": os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0"),
+    "result_backend": os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/0"),
+}
+celery.conf.update(values)
+logger.info(str(values))
