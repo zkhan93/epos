466abeffd885e92f881dfccd5793d0c87da60ca5
Zeeshan Khan <zkhan1093@gmail.com>
2022-06-12 03:41:40 +0530

    added time limit on tasks

--- worker.py
+++ worker.py
@@ -6,7 +6,8 @@ logger = logging.getLogger("worker")
 celery = Celery(__name__)
 values = {
     "broker_url": os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0"),
-    "result_backend": os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/0"),
+    "result_backend": os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/1"),
+    "task_time_limit": int(os.getenv("TASK_TIME_LIMIT", "6")),
 }
 celery.conf.update(values)
 logger.info(str(values))
